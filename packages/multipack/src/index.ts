e45d5ea4e8580ca34f5e4ac4bb5da5a15e9a04d4
happy_hunter <gherciu553@gmail.com>
2019-09-17 20:22:20 +0300

    refactor: updated utils imports

--- packages/multipack/src/index.ts
+++ packages/multipack/src/index.ts
@@ -1,7 +1,8 @@
-import { toNumber } from 'multipack-utils'
+import * as multipackUtils from 'multipack-utils'
 import { Multipack, TSum } from '../@types'
 
-const sum: TSum = (a, b) => toNumber(a) + toNumber(b)
+const sum: TSum = (a, b) =>
+  multipackUtils.toNumber(a) + multipackUtils.toNumber(b)
 
 const multipack: Multipack = {
   sum,
