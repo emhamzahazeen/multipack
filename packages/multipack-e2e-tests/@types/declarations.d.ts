1f35d9c8a6b44d1c25087bda31d25d84109fcb16
Gherciu Gheorghe <gherciu553@gmail.com>
2020-01-12 14:19:17 +0200

    Update declarations.d.ts

--- packages/multipack-e2e-tests/@types/declarations.d.ts
+++ packages/multipack-e2e-tests/@types/declarations.d.ts
@@ -1,6 +1,7 @@
-declare module 'shelljs' {
-  const content: {
-    exec: Function
-  }
-  export = content
-}
+// Declarations for makages without exposed TS types
+// declare module 'shelljs' {
+//   const content: {
+//     exec: Function
+//   }
+//   export = content
+// }
