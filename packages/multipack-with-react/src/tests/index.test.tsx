ddc0d16dd44484e2b78efaa86016ea7db79a897b
Gherciu Gheorghe <gheorghe.gherciu@endava.com>
2020-01-13 11:28:54 +0200

    feat: add tsx to lint staged

--- packages/multipack-with-react/src/tests/index.test.tsx
+++ packages/multipack-with-react/src/tests/index.test.tsx
@@ -6,7 +6,7 @@ import { renderHook, act } from '@testing-library/react-hooks'
 import useCounter from '../index'
 
 describe('useCounter', () => {
-  it('should increment counter', () => {
+  it('should increment counter ', () => {
     const { result } = renderHook(() => useCounter())
 
     act(() => {
