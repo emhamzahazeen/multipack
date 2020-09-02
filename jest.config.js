6237b4d829fb5573a89561974ebb6f733f76b345
Gherciu Gheorghe <gherciu553@gmail.com>
2020-09-02 10:53:57 +0300

    feat: implement shareable jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
-module.exports = {
-  collectCoverageFrom: ['packages/**/*.(ts|tsx)'],
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
-    },
-  },
-  testRegex: 'tests/.*\\.test\\.(ts|tsx)$',
-  preset: 'ts-jest',
-}
+const {
+  commonJestConfig,
+  tsJestConfig,
+  monorepoJestConfig,
+  mergeConfigs,
+} = require('@episclera/toolkit')
+
+module.exports = mergeConfigs(
+  commonJestConfig,
+  monorepoJestConfig,
+  tsJestConfig,
+)
