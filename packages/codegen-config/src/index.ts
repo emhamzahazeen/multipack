12b056a92cc6574a632be0c47172d1a28996adba
Gherciu Gheorghe <gherciu553@gmail.com>
2021-06-09 15:33:22 +0300

    feat: dynamic load schema url in codegen

--- packages/codegen-config/src/index.ts
+++ packages/codegen-config/src/index.ts
@@ -1,7 +1,23 @@
+import path from 'path'
+import { readFileSync, existsSync } from 'fs'
+import { parse } from 'dotenv'
 import { CodeGenConfig } from '../types'
 
+const envFilePath = path.join(process.cwd(), `./.env.${process.env.NODE_ENV}`)
+/**
+ * Using parse from dotenv instead of config because we don't want to override something from process.env
+ */
+const parsedEnvs = parse(
+  existsSync(envFilePath) ? readFileSync(envFilePath) : '',
+)
+
 const codeGenConfig: CodeGenConfig = {
-  schema: 'https://countries.trevorblades.com',
+  /**
+   * Resolving schema URI depending on available envs properties for Next and Strapi projects
+   */
+  schema:
+    parsedEnvs.NEXT_PUBLIC_API_URI ||
+    `${parsedEnvs.HOST || '0.0.0.0'}:${parsedEnvs.PORT || 3001}/graphql`,
   generates: {
     './api-types.d.ts': {
       plugins: [
--- packages/codegen-config/src/tests/index.test.ts
+++ packages/codegen-config/src/tests/index.test.ts
@@ -1,7 +1,45 @@
-import codeGenConfig from '../index'
+import path from 'path'
+import { writeFile, unlink } from 'fs/promises'
 
 describe('codeGenConfig', () => {
-  it('Should correctly re-export packages', () => {
-    expect(codeGenConfig).toBeDefined()
+  it('Should load .env file depending on NODE_ENV and read from there necessary properties to build in the end correct schema url if is a Next like project', async () => {
+    const envFilePath = path.join(__dirname, `./.env.${process.env.NODE_ENV}`)
+    const spyCwd = jest.spyOn(process, 'cwd')
+    spyCwd.mockReturnValue(__dirname)
+
+    await writeFile(
+      envFilePath,
+      'NEXT_PUBLIC_API_URI = https://api.example.com/graphql',
+    )
+
+    // Importing it dynamically because .env file also is created dynamically in this test and it can't read it initially
+    const codeGenConfig = await import('../index')
+
+    expect(codeGenConfig.default.schema).toBe('https://api.example.com/graphql')
+    await unlink(envFilePath)
+  })
+
+  it('Should load .env file depending on NODE_ENV and read from there necessary properties to build in the end correct schema url if is a Strapi like project', async () => {
+    const envFilePath = path.join(__dirname, `./.env.${process.env.NODE_ENV}`)
+    const spyCwd = jest.spyOn(process, 'cwd')
+    spyCwd.mockReturnValue(__dirname)
+
+    await writeFile(envFilePath, 'HOST = 0.0.0.0\nPORT = 3005')
+
+    // Importing it dynamically because .env file also is created dynamically in this test and it can't read it initially
+    const codeGenConfig = await import('../index')
+
+    expect(codeGenConfig.default.schema).toBe('0.0.0.0:3005/graphql')
+    await unlink(envFilePath)
+  })
+
+  it('Should not throw errors if .env file are not found and also should return a default schema url using Strapi defaults', async () => {
+    const spyCwd = jest.spyOn(process, 'cwd')
+    spyCwd.mockReturnValue('./unknownPath')
+
+    // Importing it dynamically because .env file also is created dynamically in this test and it can't read it initially
+    const codeGenConfig = await import('../index')
+
+    expect(codeGenConfig.default.schema).toBe('0.0.0.0:3001/graphql')
   })
 })
